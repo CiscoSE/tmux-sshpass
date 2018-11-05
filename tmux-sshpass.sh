86860b4a01d623c359a24bf9c94b3e9b5716627e
Ed Jackson <edjacks@cisco.com>
2018-11-05 13:56:10 -0500

    changed password

--- tmux-sshpass.sh
+++ tmux-sshpass.sh
@@ -12,7 +12,7 @@ for y in 23 33; do
   for x in $(seq 1 7; seq 50 51; seq 211 216); do
 
     tmux send-keys -t test:0.0 C-m C-m C-m "# -->192.${y}.22.${x}<--" C-m C-m
-    tmux send-keys -t test:0.0 "/usr/bin/sshpass -p password /usr/bin/ssh cisco@10.${y}.20.${x}" C-m
+    tmux send-keys -t test:0.0 "/usr/bin/sshpass -p Passw0rd /usr/bin/ssh cisco@10.${y}.20.${x}" C-m
     sleep 5
     tmux send-keys -t test:0.0 "copy run start" C-m C-m
     sleep 5
